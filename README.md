ae087b10ccc62b2e79817ace05e31ff2ba63ad7a
Kouadio Kouao <kouaokouadio@gmail.com>
2023-11-30 21:05:22 -0700

    final

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # weather-dashboard
 The Weather Dashboard is an interactive web application designed to provide real-time weather information. It aims to deliver accurate and up-to-date weather forecasts, including temperature, humidity, wind speed, and more, for any specified location. 
+
+
+Weather Dashboard
+Project Overview
+The Weather Dashboard is a web-based application designed to provide real-time weather information and tips based on the user's chosen city. It utilizes the OpenWeatherMap API to fetch current weather data and displays it alongside custom tips from a local JSON file.
+
+Features
+City-based Weather Information: Users can search for weather information by entering a city name.
+Real-time Data: The application displays real-time data including temperature, humidity, and wind speed.
+Weather Tips: Provides custom tips based on the current weather conditions, sourced from a local JSON file (weather_tips.json).
+Error Handling: Displays user-friendly error messages for invalid city entries or data fetch issues.
+How to Use
+Clone/Download the Repository:
+Clone this repository to your local machine or simply download the source code.
+Open the Project:
+Open the project folder in your preferred IDE/text editor.
+Run a Local Server (Optional):
+If you are testing locally, it's recommended to run the project on a local server due to browser security restrictions with local files.
+Open index.html:
+Run the index.html file in a web browser to start the application.
+Technology Stack
+HTML: Structure of the web pages.
+CSS: Styling of the web pages.
+JavaScript: Dynamic functionalities, API requests, and handling JSON data.
+API Reference
+OpenWeatherMap API: Used for fetching real-time weather data.
+JSON Data Format
+The weather_tips.json file should follow this format:
+
+json
+Copy code
+{
+  "defaultTip": "General tip for any weather condition",
+  "tips": {
+    "WeatherCondition1": "Specific tip for WeatherCondition1",
+    "WeatherCondition2": "Specific tip for WeatherCondition2",
+    ...
+  }
+}
+License
+This project is open source and available under the MIT License.
+
+Acknowledgements
+Weather data provided by OpenWeatherMap.
--- script.js
+++ script.js
@@ -1,34 +1,55 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('searchForm');
     form.addEventListener('submit', fetchWeather);
+    loadWeatherTips();
 });
+
+let weatherTips = {};
+
+const loadWeatherTips = async () => {
+    try {
+        const response = await fetch('weather_tips.json');
+        weatherTips = await response.json();
+    } catch (error) {
+        console.error('Error loading weather tips:', error);
+    }
+};
 
 const fetchWeather = async (event) => {
     event.preventDefault();
     const city = document.getElementById('searchInput').value;
-    const apiKey = 'e8c1a01a0e145bf881d30a0713fb5e7a'; // API key
+    const apiKey = 'e8c1a01a0e145bf881d30a0713fb5e7a'; // Replace with your actual API key
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     try {
         const response = await fetch(apiUrl);
         if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+            throw new Error('City not found');
         }
         const weatherData = await response.json();
         displayWeather(weatherData);
     } catch (error) {
-        console.error('Error fetching weather data:', error);
-        // Optionally, display this error to the user
+        displayError(error.message);
     }
 };
 
 const displayWeather = (data) => {
     const displayDiv = document.getElementById('weatherDisplay');
+    const weatherCondition = data.weather[0].main;
+
+    let tip = weatherTips.tips[weatherCondition] || weatherTips.defaultTip;
+
     displayDiv.innerHTML = `
         <h2>Weather in ${data.name}</h2>
-        <p>Temperature: ${data.main.temp}°C</p>
-        <p>Weather: ${data.weather[0].main}</p>
+        <p>Temperature: ${data.main.temp.toFixed(2)}°C</p>
+        <p>Weather: ${weatherCondition}</p>
         <p>Humidity: ${data.main.humidity}%</p>
-        <p>Wind Speed: ${data.wind.speed}m/s</p>
+        <p>Wind Speed: ${data.wind.speed.toFixed(2)}m/s</p>
+        <p><strong>Tip:</strong> ${tip}</p>
     `;
+};
+
+const displayError = (message) => {
+    const displayDiv = document.getElementById('weatherDisplay');
+    displayDiv.innerHTML = `<p class="error-message">${message}</p>`;
 };
